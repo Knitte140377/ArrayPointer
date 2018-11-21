9c0c3d77a16bf9e18061b09d3700e88cd71e7c7a
Markus <43406203+Knitte140377@users.noreply.github.com>
2018-11-21 12:59:54 +0100

    Update zahlensys.c

--- zahlensys.c
+++ zahlensys.c
@@ -15,7 +15,7 @@ int zehnerzahl=0;
 for(int i=stringSize;i>=0;i--) //zu zehn machen
 {
     wandler = (int)string[i];
-    if(wandler>64)  //Ascii in azahlenwert
+    if(wandler>64)  //Ascii in 10er compiler
     {wandler=wandler-55;}else{wandler=wandler-48;} 
     potenz =stringSize-i;
     zehnerzahl =wandler*pow(zahlensystem, potenz)+zehnerzahl;
@@ -24,19 +24,40 @@ for(int i=stringSize;i>=0;i--) //zu zehn machen
 printf("16er System %s\n",string);
 printf("10er System: %d\n",zehnerzahl);
 
-int rest=0;
-rest=zehnerzahl%16;
+int rest;
+int index=0;
+int zielsystem=16;
+char ergebnisVerkehrt[] = "0";
 
-zehnerzahl=zehnerzahl/16;
-    if(wandler>9)  //Ascii in zahlenwert
-    {wandler=wandler+55;}else{wandler=wandler+48;}
-    printf("%c\n",(char)wandler);
-    string[2]=(char)wandler;
+while(zehnerzahl>zielsystem)
+{
+    rest=zehnerzahl%zielsystem;
+    zehnerzahl=zehnerzahl/zielsystem;
+printf("zehnerzahl %d rest %d\n",zehnerzahl,rest);
+ 
+ if(rest>9)
+    {rest=rest+55;}else{rest=rest+48;} 
+    ergebnisVerkehrt[index]=(char)rest;
+    index=index+1;
+    printf("%s\n",ergebnisVerkehrt);
+}
+    rest=zehnerzahl;
+ if(rest>9)    
+    {rest=rest+55;}else{rest=rest+48;} 
+    ergebnisVerkehrt[index]=(char)rest;
+    printf("%s\n",ergebnisVerkehrt);
+printf("zehnerzahl %d rest %d\n",zehnerzahl,rest);
 
-printf("rest %d\n",rest);
+printf("ergebnis %s \n",ergebnisVerkehrt);
 
+char ergebnis[]="0";
 
-printf("16er System %s\n",string);
-
+int chars=index;
+for(int i=0;i=chars;i++)
+{
+ergebnis[i]=ergebnisVerkehrt[index];
+index=index-1;
+}
+printf("ergebnis %s \n",ergebnis);
 return 0;
 }
